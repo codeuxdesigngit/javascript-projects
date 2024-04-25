18413d66eabce13b9b0e88f47c6488c32723865f
Julia Milton <jkuprina@gmail.com>
2024-04-25 04:38:37 -0500

    Update data-and-variables-exercises.js

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -3,7 +3,7 @@
 let shuttleName = 'Determination';
 let shuttleSpeedMph = 17500;
 let distanceToMarsKm = 225000000;
-let distanceToMoonKm = 38400;
+let distanceToMoonKm = 384400;
 const milesPerKm = 0.621;
 
 
